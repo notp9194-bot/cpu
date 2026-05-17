1f9dedc3b8503831df089d40f3eddb1af5fd79da
Tumhara Naam <tumhara-email@example.com>
2026-05-18 05:21:35 +0530

    Replace DeviceInfo with latest feature version

--- DeviceInfo/core/build.gradle.kts
+++ DeviceInfo/core/build.gradle.kts
@@ -7,10 +7,10 @@ android {
     compileSdk = 35
     defaultConfig { minSdk = 24 }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions { jvmTarget = "1.8" }
+    kotlinOptions { jvmTarget = "17" }
     buildFeatures { viewBinding = true }
 }
 dependencies {
--- DeviceInfo/feature-memory/build.gradle.kts
+++ DeviceInfo/feature-memory/build.gradle.kts
@@ -8,10 +8,10 @@ android {
     compileSdk = 35
     defaultConfig { minSdk = 24 }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions { jvmTarget = "1.8" }
+    kotlinOptions { jvmTarget = "17" }
     buildFeatures { viewBinding = true }
 }
 
